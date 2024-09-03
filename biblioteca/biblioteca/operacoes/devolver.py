d778de75816f4e292ececd57154bafb4f941aeaf
Mariana Penalva <marianapenalva07@gmail.com>
2024-09-03 00:30:06 -0300

    Delete biblioteca/biblioteca directory

--- biblioteca/biblioteca/operacoes/devolver.py
+++
@@ -1,8 +0,0 @@
-from utils.banco import dicio
-
-def devolver_livro():
-    livro = str(input("Digite o nome do livro a ser devolvido: "))
-    if livro in dicio:
-        print(f"O livro '{livro}' foi devolvido com sucesso.")
-    else:
-        print(f"O livro '{livro}' não está registrado no acervo.")
